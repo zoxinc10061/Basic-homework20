9347c971e3e727773dae0b055dd4ded5fb0fdcac
zoxinc10061 <48340647+zoxinc10061@users.noreply.github.com>
2019-05-24 10:43:03 +0800

    Add files via upload

---
+++ homework-20/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>  
+#include <stdlib.h> 
+
+struct phone 
+
+{
+
+   char brand[16],type[10];
+
+   int year,price;
+
+}; 
+
+int main(void)
+
+{
+
+    int i;
+
+	printf("-------------------------------------------------\n");
+
+	printf("                 phone手機結構體                 \n");
+
+	printf("-------------------------------------------------\n"); 
+
+    struct phone p[5]={  
+
+	"APPLE", "X", 2017, 35000, 
+
+    "Samsung", "S9", 2018, 30000, 
+
+    "Huawei", "P20 Pro", 2018, 32000, 
+
+    "OPPO", "R15", 2018, 9000, 
+
+    "ASUS", "Zenfone 5", 2018, 12000
+
+	};
+
+    
+
+    for (i=0; i<5; i++) 
+
+    {
+
+        printf("%s %s %4d %5d\n", p[i].brand, p[i].type, p[i].year, p[i].price);
+
+    }
+
+    system("pause"); 
+
+    return 0;
+
+}
